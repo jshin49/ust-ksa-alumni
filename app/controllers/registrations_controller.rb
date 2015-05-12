5d96d3bb40a298eed689d4d40a5e2d01406d93a4
jihopark <lastspurt17@gmail.com>
2015-05-12 18:14:35 +0800

    Added Checkboxes for Major/Interests in Edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
  
+ def edit
+  @ssc = Major.where(school: "School of Science")
+  @seng = Major.where(school: "School of Engineering")
+  @sbm = Major.where(school: "School of Business")
+  @huma = Major.where(school: "School of Humanities and Social Science")
+  @ipo = Major.where(school: "Interdisciplinary Programs Office")
+  
+  @favorite_industries = Industry.where(favorite:true)
+  @other_industries = Industry.where(favorite:false)
+  
+ end
+ 
  private
 
   def sign_up_params

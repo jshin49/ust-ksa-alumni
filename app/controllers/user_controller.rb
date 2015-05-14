8081ca653eaa30ccce5f0908ab35fd6df49831ff
jihopark <lastspurt17@gmail.com>
2015-05-14 13:57:28 +0800

    Fill Major Select in User#index

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,20 @@
 class UserController < ApplicationController
   def index  
-    @alumni = User.where(status: "alumni")     
+    @alumni = User.where(status: "alumni")
+    
+    @majors = get_majors_with_declaration     
+    
     render layout: 'layouts/user_index'
+  end
+  
+  private 
+  
+  def get_majors_with_declaration
+    unique_major_ids = Declaration.distinct.pluck(:major_id)
+    unique_majors = []
+    unique_major_ids.each do |id|
+      unique_majors << Major.find_by_id(id)
+    end
+    return unique_majors.sort! {|x,y| x.id <=> y.id}
   end
 end

d1d071650eac16680a4fa0e287a61d22ccecafe8
jihopark <lastspurt17@gmail.com>
2015-09-16 15:16:17 +0800

    Disable UST Domain validation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,10 +1,10 @@
 class Invitation < ActiveRecord::Base
-	belongs_to :user	
+	belongs_to :user
 	before_create :create_secret_key
-	validates :email, format: {with: /\A([\w\.%\+\-]+)(@ust\.hk\z)|(@connect\.ust\.hk\z)|(@stu\.ust\.hk\z)/i, message: "can invite only UST domains. Ex. connect.ust.hk"} 
-	
+	#validates :email, format: {with: /\A([\w\.%\+\-]+)(@ust\.hk\z)|(@connect\.ust\.hk\z)|(@stu\.ust\.hk\z)/i, message: "can invite only UST domains. Ex. connect.ust.hk"}
+
 	private
-	
+
 	def create_secret_key
 	  begin
     	self.secret_key = SecureRandom.hex(5)
